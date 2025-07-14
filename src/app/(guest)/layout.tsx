a643a282ad948fa981aacf45b8ab6055499f1040
Mohamed Mushkir <mushkir@titumlk.onmicrosoft.com>
2025-07-14 11:24:51 +0530

    style: designed sidebar layout

--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <TheNavbar />
           {children}
         </ThemeProvider>
       </body>
